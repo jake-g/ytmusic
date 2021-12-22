6859bb0737552753b85146fa3ba027085d20a0f8
jake-g <omonoid@gmail.com>
2021-12-21 21:57:38 -0800

    cleanup path name change

--- run_backup.bat
+++ run_backup.bat
@@ -4,7 +4,7 @@ python backup_playlists.py
 git add playlists/*.tsv
 git commit -m "update playlists"
 
-CD /D D:\Projects\_Projects_Synced\music-library\unified_music_db
+CD /D D:\Projects\_Projects_Synced\music-library\music-sources-unified
 python match_ytmusic_tracks_with_musicbee.py
 git add *.tsv
 git commit -m "update tsvs"
