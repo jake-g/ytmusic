487d3a35f18bda38b1cf35aa2aa9c505d84ac42d
jake-g <omonoid@gmail.com>
2022-11-06 20:18:33 -0800

    manually organized and renamed playlists, then tested playlist prune notebook to process each playlist. added and test prune playlist notebook, ported some of api notebook into this new one

--- ytmusic_library.py
+++ ytmusic_library.py
@@ -3,16 +3,26 @@ import time
 import pandas as pd
 from ytmusicapi import YTMusic
 
-PLAYLIST_LIMIT=5000
+PLAYLIST_LIMIT = 6000
+MIN_RADIO_LIKE_TO_SPLIT = 8
+VALID_TRACK_RATINGS = ('LIKE', 'DISLIKE', 'INDIFFERENT', 'NONE')
+VALID_PLAYLIST_KINDS = ('LIKE', 'NOT_LIKE', 'INDIFFERENT',
+                        'ALBUM', 'SKIP', 'YT_GENERATED')
+
+# regnerate playlists with more than this amount of duplicates
+DUPLICATE_THRESHOLD = 3
+
 
 class YTMusicPlaylists:
 
-    def __init__(self, header='headers_auth.json', playlist_limit=PLAYLIST_LIMIT):
+    def __init__(self, header='headers_auth.json',
+                 playlist_limit=PLAYLIST_LIMIT):
         self.yt = YTMusic(header)
         self.playlist_limit = playlist_limit
         self.playlists = pd.DataFrame(
             self.yt.get_library_playlists(limit=playlist_limit))
-        self._valid_ratings = ('LIKE', 'DISLIKE', 'INDIFFERENT')
+        self._valid_ratings = VALID_TRACK_RATINGS
+        self._valid_playlist_kinds = VALID_PLAYLIST_KINDS
         self._info_cache = {}
 
     def _playlist_loc_first(self, col, value):
@@ -20,7 +30,8 @@ class YTMusicPlaylists:
         if len(res) == 0:
             print(f'No playlist with {col}: {value}')
         elif len(res) > 1:
-            print(f'multiple matches for : {value}, choosing first result of:\n {res}')
+            print(f'Multiple matches for: {value},',
+                  f'choosing first result of:\n {res}')
         return res.iloc[0]
 
     def query_by_title(self, title):
@@ -30,7 +41,8 @@ class YTMusicPlaylists:
         return self._playlist_loc_first(col='playlistId', value=playlistId)
 
     def get_playlists_by_privacy(self, privacy='PUBLIC',
-                                 skip_if_contains=('z_', 'zz_', 'zzz_')):
+                                 skip_if_contains=('z_', 'zz_',
+                                                   'zzz_', 'yyz_')):
         out_playlists = []
         for i, p in self.playlists.iterrows():
             if len(p) == self.playlist_limit:
@@ -61,7 +73,7 @@ class YTMusicPlaylists:
         pl_name = os.path.basename(tsv_path).split('.tsv')[0]
         print(f'\nGenerating {pl_name} ytmusic playlist for {len(df)} tracks')
         vids = df.videoId.unique().tolist()
-        desc = f'Matched {len(vids)} tracks from {pl_name} manually uploaded from local tsv.'
+        desc = f'Matched {len(vids)} tracks from local tsv playlist: {pl_name}'
         pl_id = self.yt.create_playlist(
             title=pl_name,  description=desc,
             privacy_status='PRIVATE', video_ids=list(vids))
@@ -87,48 +99,61 @@ class YTMusicPlaylists:
         tracks = self.parse_tracks(track_list)
         return tracks, playlist_meta
 
-    def create_rating_playlist_subset(self, tracks, name, rating, min_ids=0):
+    def create_rating_playlist_subset(self, tracks, orig_name,
+                                      new_name, rating, min_ids=0):
         assert rating in self._valid_ratings
         filtered_tracks = tracks.loc[tracks['likeStatus'] == rating]
         video_ids = filtered_tracks['videoId'].unique().tolist()
-        if len(video_ids) > min_ids:
-            pl_id = self.yt.create_playlist(
-                title=name + ' ' + rating.lower(),
-                description='generated from %s includes %s subset' % (
-                    name, rating),
-                privacy_status='PRIVATE',
-                video_ids=video_ids
-            )
-            return pl_id
+        if len(video_ids) <= min_ids:
+            return None, len(video_ids)
+        desc = f'generated from {orig_name} includes {rating} subset'
+        pl_id = self.yt.create_playlist(
+            title=new_name, description=desc,
+            privacy_status='PRIVATE', video_ids=video_ids
+        )
+        return pl_id, len(video_ids)
 
-    def create_like_and_unrated_rating_playlist_subset(self, playlist_name,
-                                                       verbose=False,
-                                                       sleep_time=10):
-        playlist = self.query_by_title(playlist_name)
+    def move_likes_from_radio_playlist(self, pl_info,
+                                       min_n_like=MIN_RADIO_LIKE_TO_SPLIT,
+                                       sleep_time=3, verbose=False):
+        tracks, metadata = self.parse_playlist(pl_info)
         if verbose:
-            print(f'Sorting {playlist_name} ({playlist["playlistId"]}) into like and indifferent')
-        tracks, metadata = self.parse_playlist(
-            playlist_meta=self.playlist_get_info(playlist["playlistId"]))
-        res_indif = self.create_rating_playlist_subset(
-            tracks, playlist_name, 'INDIFFERENT')
+            print(f'Sorting {pl_info["title"]} ({pl_info["id"]}) with',
+                  f'{len(tracks)} tracks into like and indifferent')
+        orig_name = pl_info["title"]
+        like_pl_name = orig_name.replace('radio', '') + 'like'
+        res_like, n_like = self.create_rating_playlist_subset(
+            tracks, orig_name, like_pl_name, 'LIKE',
+            min_ids=min_n_like
+        )
+        if res_like is None:
+            print(f'Not splitting playlist {orig_name}, ',
+                  f'not enough likes ({n_like})')
+            return
         time.sleep(sleep_time)
-        res_like = self.create_rating_playlist_subset(
-            tracks, playlist_name, 'LIKE')
+        indiff_pl_name = orig_name.replace('radio', '') + 'radio'
+        res_indif, n_indif = self.create_rating_playlist_subset(
+            tracks, orig_name, indiff_pl_name, 'INDIFFERENT', min_ids=0)
+        if res_indif:
+            print(f'ERROR: Radio Playlist {indiff_pl_name} was',
+                  f'not created, skip deleting original : {orig_name}')
+            return
         time.sleep(sleep_time)
-        self.yt.delete_playlist(playlist['playlistId'])
-        if verbose:
-            print(f'Created like playlist ({res_like}) and unrated playlist ({res_indif})\nDeleted original playlist: {playlist_name} ({playlist["playlistId"]})')
+        self.yt.delete_playlist(pl_info["id"])
+        print(f'Created like playlist: {like_pl_name} ({n_like}) and',
+              f'indifferent playlist: {indiff_pl_name} ({n_indif} entries),',
+              f'deleted original playlist: {orig_name} ({len(tracks)} entries)')
 
-    def playlist_rate_all_songs(self, playlistId, rating,
-                                sleep_time=0.5, verbose=False):
+    def playlist_rate_all_songs(self, pl_info, rating, sleep_time=0.5,
+                                verbose=False, skip_if_dislike=False):
         assert rating in self._valid_ratings
-        playlist = self.query_by_playlistId(playlistId)
-        playlist_meta = self.playlist_get_info(playlistId)
-        num_tracks = len(playlist_meta["tracks"])
         if verbose:
-            print(f'Found {num_tracks} tracks to rate as {rating} in playlist: {playlist["title"]}')
+            print(f'Playlist {pl_info["title"]}: Found',
+                  f'{len(pl_info["tracks"])} tracks to rate as {rating}')
         rate_count = 0
-        for track in playlist_meta["tracks"]:
+        for track in pl_info["tracks"]:
+            if skip_if_dislike and track["likeStatus"] == 'DISLIKE':
+                continue
             if track["likeStatus"] == rating:
                 continue
             if verbose:
@@ -136,27 +161,31 @@ class YTMusicPlaylists:
             self.yt.rate_song(track["videoId"], rating=rating)
             rate_count += 1
             time.sleep(sleep_time)
-        print(f'Rated {rate_count} of {num_tracks} tracks as {rating} in playlist {playlist["title"]}')
+        print(f'Playlist {pl_info["title"]}: Rated {rate_count}',
+              f'of {len(pl_info["tracks"])} tracks as {rating}')
 
-    def _playlist_is_dislike(self, name):
-        name = name.lower()
-        if 'not like' in name:
-            return True
-        if ' dislike' in name:
-            return True
-        if 'thumbs_down' in name:
-            return True
-
-    def _playlist_is_like(self, name):
-        name = name.lower()
-        if self._playlist_is_dislike(name):
-            return False
-        if 'thumbs_up' in name:
-            return True
-        if ' like' in name or ' likes' in name:
-            return True
-        if ' top' in name:
-            return True
+    def playlist_remove_duplicates(self, pl_info,
+                                   duplicate_threshold=DUPLICATE_THRESHOLD,
+                                   sleep_time=0.5, verbose=False):
+        if verbose:
+            print(f'Playlist {pl_info["title"]}: Found {pl_info["tracks"]}',
+                  'tracks to check for duplicates')
+        track_ids = [t['videoId'] for t in pl_info['tracks']]
+        tracks_unique = frozenset(track_ids)
+        n_dupes = len(track_ids)-len(tracks_unique)
+        if n_dupes >= duplicate_threshold:
+            new_id = self.yt.create_playlist(
+                title=str(pl_info['title']),
+                description=str(pl_info['description']),
+                video_ids=tracks_unique
+            )
+            time.sleep(sleep_time)
+            self.yt.delete_playlist(playlistId=pl_info['id'])
+            time.sleep(sleep_time)
+            print(f'Playlist {pl_info["title"]}: {n_dupes} duplicate tracks',
+                  f'removed ({len(tracks_unique)} of {len(track_ids)} unique)')
+            return new_id
+        return pl_info['id']
 
     def playlist_get_all_like_playlists(self):
         like_playlists_ids = {}
@@ -164,3 +193,70 @@ class YTMusicPlaylists:
             if self._playlist_is_like(row.title.lower()):
                 like_playlists_ids[row.title] = row.playlistId
         return like_playlists_ids
+
+    def infer_playlist_kind(self, p_row):
+        if str(p_row.author) == 'nan':  # (auto generated by yt)
+            return 'YT_GENERATED'
+        elif self._playlist_is_albums(p_row.title):
+            return 'ALBUM'
+        elif self._playlist_skip_starts_with(p_row.title):
+            return 'SKIP'
+        elif self._playlist_is_radio(p_row.title):
+            return 'INDIFFERENT'
+        elif self._playlist_is_dislike(p_row.title):
+            return 'NOT_LIKE'
+        elif self._playlist_is_like(p_row.title):
+            return 'LIKE'
+        return None  # handle this case when using result
+
+    def _is_playlist_kind_ok(self, pl_kind, like_percent,
+                             like_min_pct=80, notlike_max_pct=20,
+                             radio_max_pct=50):
+        # Not OK Cases
+        if pl_kind == 'LIKE' and like_percent <= like_min_pct:
+            return False
+        elif pl_kind == 'RADIO' and like_percent >= radio_max_pct:
+            return False
+        elif pl_kind == 'NOT_LIKE' and like_percent >= notlike_max_pct:
+            return False
+        return True
+
+    def _playlist_is_dislike(self, name,
+                             dislike_toks=('not like', ' dislike',
+                                           'thumbs_down')):
+        name = name.lower()
+        for tok in dislike_toks:
+            if tok in name:
+                return True
+
+    def _playlist_is_like(self, name,
+                          like_toks=('thumbs_up', ' like',
+                                     ' top', ' likes')):
+        name = name.lower()
+        if self._playlist_is_dislike(name):
+            return False
+        for tok in like_toks:
+            if tok in name:
+                return True
+
+    def _playlist_is_radio(self, name,
+                           radio_toks=(' radio', '_radio',
+                                       '_indifferent')):
+        name = name.lower()
+        if self._playlist_is_like(name):
+            return False
+        for tok in radio_toks:
+            if tok in name:
+                return True
+
+    def _playlist_is_albums(self, name, album_toks=('_albums', '  album')):
+        name = name.lower()
+        for tok in album_toks:
+            if tok in name:
+                return True
+
+    def _playlist_skip_starts_with(self, name, start_toks=('zz_')):
+        name = name.lower()
+        for tok in start_toks:
+            if name.startswith(tok):
+                return True
