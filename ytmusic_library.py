7682f458125f6327c103f2c6a1d03e3c97b4bdf9
jake-g <omonoid@gmail.com>
2023-01-22 10:41:05 -0800

    upload 2022 new subreddit tracks to playlists (albums still todo)

--- ytmusic_library.py
+++ ytmusic_library.py
@@ -163,7 +163,7 @@ class YTMusicPlaylists:
             print(f'Sorting {pl_info["title"]} ({pl_info["id"]}) with',
                   f'{len(tracks)} tracks into like and indifferent')
         orig_name = pl_info["title"]
-        like_pl_name = orig_name.replace('radio', '') + 'like'
+        like_pl_name = orig_name.replace('_radio', '') + '_like'
         res_like, n_like = self.create_rating_playlist_subset(
             tracks, orig_name, like_pl_name, 'LIKE',
             min_ids=min_n_like
@@ -173,7 +173,7 @@ class YTMusicPlaylists:
                   f'not enough likes ({n_like})')
             return
         time.sleep(sleep_time)
-        indiff_pl_name = orig_name.replace('radio', '') + 'radio'
+        indiff_pl_name = orig_name.replace('_radio', '') + '_radio'
         res_indif, n_indif = self.create_rating_playlist_subset(
             tracks, orig_name, indiff_pl_name, 'INDIFFERENT', min_ids=0, playcount_sort=playcount_sort,
             ignore_banned=ignore_banned)
